d6ea3cf9ecb4651a6ce4967de6827bbabeb4813c
NatashaZlenko <natazlyzy@gmail.com>
2022-02-02 23:49:50 +0200

    деструктуризация

--- js/script04.js
+++ js/script04.js
@@ -67,102 +67,102 @@
 
 
 
-// console.log('');
-// console.log('Метод Object.keys()');
-// console.log('');
+// // console.log('');
+// // console.log('Метод Object.keys()');
+// // console.log('');
 
-// const keys = Object.keys(book);
+// // const keys = Object.keys(book);
 
-// for (const key of keys) {
-//     console.log(key);
-//     console.log(book[key]);
+// // for (const key of keys) {
+// //     console.log(key);
+// //     console.log(book[key]);
+// // };
+
+
+
+// // console.log('');
+// // console.log('Метод Object.values()');
+
+// // const keys1 = Object.keys(book);
+// // const values = Object.values(book);
+
+// // console.log(keys1, values);
+
+
+// // const goods = {
+// //     apples: 6,
+// //     grapes: 3,
+// //     bread: 4,
+// //     cheese: 7,
+// // };
+
+// // const values1 = Object.values(goods);
+
+// // let total = 0;
+
+// // for (const value of values1) {
+// //     total += value
+// // };
+
+// // console.log(total);
+
+
+// // console.log('');
+// // console.log('Метод Object.entries()');
+
+// // const entries = Object.entries(book);
+// // console.log(entries);
+
+
+
+// // ================== Массив обьектов
+
+// const books = [
+//     {
+//       title: "The Last Kingdom",
+//       author: "Bernard Cornwell",
+//       rating: 8.38,
+//     },
+//     {
+//       title: "На берегу спокойных вод",
+//       author: "Роберт Шекли",
+//       rating: 8.51,
+//     },
+//     {
+//       title: "Сон смешного человека",
+//       author: "Федор Достоевский",
+//       rating: 7.75,
+//     },
+// ];
+
+// for (const book of books) {
+//     console.log(book);
+//     console.log(book.title);
+//     console.log(book.author);
+//     console.log(book.rating);
 // };
 
 
-
 // console.log('');
-// console.log('Метод Object.values()');
+// console.log('список названий всех книг');
 
-// const keys1 = Object.keys(book);
-// const values = Object.values(book);
+// const bookNames = [];
 
-// console.log(keys1, values);
+// for (const book of books) {
+//     bookNames.push(book.title);
+// }
 
-
-// const goods = {
-//     apples: 6,
-//     grapes: 3,
-//     bread: 4,
-//     cheese: 7,
-// };
-
-// const values1 = Object.values(goods);
-
-// let total = 0;
-
-// for (const value of values1) {
-//     total += value
-// };
-
-// console.log(total);
+// console.log(bookNames);
 
 
 // console.log('');
-// console.log('Метод Object.entries()');
+// console.log('средний рейтинг всей коллекции');
 
-// const entries = Object.entries(book);
-// console.log(entries);
+// let totalRating = 0;
 
+// for (const book of books) {
+//     totalRating += book.rating;
+// };
 
-
-// ================== Массив обьектов
-
-const books = [
-    {
-      title: "The Last Kingdom",
-      author: "Bernard Cornwell",
-      rating: 8.38,
-    },
-    {
-      title: "На берегу спокойных вод",
-      author: "Роберт Шекли",
-      rating: 8.51,
-    },
-    {
-      title: "Сон смешного человека",
-      author: "Федор Достоевский",
-      rating: 7.75,
-    },
-];
-
-for (const book of books) {
-    console.log(book);
-    console.log(book.title);
-    console.log(book.author);
-    console.log(book.rating);
-};
-
-
-console.log('');
-console.log('список названий всех книг');
-
-const bookNames = [];
-
-for (const book of books) {
-    bookNames.push(book.title);
-}
-
-console.log(bookNames);
-
-
-console.log('');
-console.log('средний рейтинг всей коллекции');
-
-let totalRating = 0;
-
-for (const book of books) {
-    totalRating += book.rating;
-};
-
-const averageRating = (totalRating / books.length).toFixed(1);
-console.log(averageRating);
+// const averageRating = (totalRating / books.length).toFixed(1);
+// console.log(averageRating);
